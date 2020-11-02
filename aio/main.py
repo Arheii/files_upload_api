7d0ad02890904e051dcdd70587614ccd01dc1fe2
Arheii <arheyy@gmail.com>
2020-11-02 08:14:28 +0000

    add post handler

--- aio/main.py
+++ aio/main.py
@@ -5,15 +5,73 @@ TODO:   Project structure
 
 
 """
-import os
-from aiohttp import web
 import pathlib
+import hashlib
+from aiohttp import web
+
+
+PROJ_ROOT = pathlib.Path(__file__).parent
+UPLOAD_FOLDER = PROJ_ROOT / 'store'
+pathlib.Path.mkdir(UPLOAD_FOLDER, exist_ok=True)
 
 routes = web.RouteTableDef()
 
 
+@routes.post('/drweb/aiohttp/storage')
+async def upload(request):
+    """Upload file from post to server folder"""
+    # TODO: request.post() -> request.multipart()
+    data = await request.post()
+    attach = data.get('file')
+
+    if attach is None:
+        raise web.HTTPBadRequest(text='no file')
+
+    # filename = attach.filename
+    file = attach.file
+
+    file_hash = hashlib.sha224(file.read()).hexdigest()
+    full_path = UPLOAD_FOLDER / file_hash[:2] / file_hash
+
+    if not full_path.exists():
+        # Create subfolder by first two symbols and save file
+        pathlib.Path.mkdir(UPLOAD_FOLDER / file_hash[:2], exist_ok=True)
+        file.seek(0)
+        # TODO: async file saving
+        with open(full_path, 'wb') as f:
+            f.write(file.read())
+
+    return web.json_response({'hash': file_hash})
+
+
+@routes.get('/drweb/aiohttp/storage')
+async def storage(request):
+    """return or delete file by its hash"""
+    file_hash = request.args.get("hash")
+
+    # Hash is there and contains only correct characters
+    if file_hash is None or not file_hash.isalnum():
+        return 'need correct hash', 422
+
+    full_path = os.path.join(UPLOAD_FOLDER, file_hash[:2], file_hash)
+
+    # Check file is exist
+    if not os.path.exists(full_path):
+        return "file with this hash not found", 404
+        
+    return web.Response(text="Hello, world")
+
+
+
+@routes.delete('/drweb/aiohttp/storage')
+async def delele(request):
+    data = {'some': 'data'}
+    return web.json_response(data)
+
+
 @routes.get('/drweb/aiohttp')
 async def test_form(request):
+    """For testing post query"""
     return web.Response(text="""
                         <!doctype html>
                         <title>Upload new File</title>
@@ -28,56 +86,8 @@ async def test_form(request):
                         content_type='text/html')
 
 
-
-@routes.get('/drweb/aiohttp/storage')
-async def storage(request):
-    return web.Response(text="Hello, world")
-
-
-@routes.post('/drweb/aiohttp/storage')
-async def upload(request):
-    return web.json_response({'hash': 'file_hash'})
-
-
-@routes.delete('/drweb/aiohttp/storage')
-async def delele(request):
-    data = {'some': 'data'}
-    return web.json_response(data)
-
-
-# async def store_mp3_handler(request):
-
-#     reader = await request.multipart()
-
-#     # /!\ Don't forget to validate your inputs /!\
-
-#     # reader.next() will `yield` the fields of your form
-
-#     field = await reader.next()
-#     assert field.name == 'name'
-#     name = await field.read(decode=True)
-
-#     field = await reader.next()
-#     assert field.name == 'mp3'
-#     filename = field.filename
-#     # You cannot rely on Content-Length if transfer is chunked.
-#     size = 0
-#     with open(os.path.join('/spool/yarrr-media/mp3/', filename), 'wb') as f:
-#         while True:
-#             chunk = await field.read_chunk()  # 8192 bytes by default.
-#             if not chunk:
-#                 break
-#             size += len(chunk)
-#             f.write(chunk)
-
-#     return web.Response(text='{} sized of {} successfully stored'
-#                              ''.format(filename, size))
-
 def main(argv):
-    PROJ_ROOT = pathlib.Path(__file__).parent
 
-    UPLOAD_FOLDER = PROJ_ROOT / 'store'
-    pathlib.Path.mkdir(UPLOAD_FOLDER, exist_ok=True)
 
     app = web.Application()
     app.add_routes(routes)
